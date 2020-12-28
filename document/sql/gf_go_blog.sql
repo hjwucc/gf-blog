e5b772710565806a07e235195dde6aefa6fb0f8d
wujj <wujj@veewap.com>
2020-12-28 19:01:55 +0800

    新增:sql文件

---
+++ document/sql/gf_go_blog.sql
@@ -0,0 +1,92 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : wubazx
+ Source Server Type    : MySQL
+ Source Server Version : 50732
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : gf_go_blog
+
+ Target Server Type    : MySQL
+ Target Server Version : 50732
+ File Encoding         : 65001
+
+ Date: 28/12/2020 18:59:42
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for articles
+-- ----------------------------
+DROP TABLE IF EXISTS `articles`;
+CREATE TABLE `articles`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '文章标题',
+  `summary` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '文章简介绍',
+  `category_id` int(11) NULL DEFAULT 1 COMMENT '文章栏目ID',
+  `tags` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '文章标签集合',
+  `cover` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '文章封面',
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '文章内容',
+  `md_content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章markdown内容',
+  `click_count` int(11) NULL DEFAULT 1 COMMENT '文章点击数',
+  `is_top` tinyint(1) NULL DEFAULT 0 COMMENT '文章置顶:0-不置顶/1-置顶',
+  `from` tinyint(1) NOT NULL DEFAULT 0 COMMENT '文章来源:0-原创/1-转载/2-其他',
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '文章状态:1-发布/2-草稿/3-隐藏',
+  `created_at` timestamp(0) NOT NULL COMMENT '创建时间',
+  `updated_at` timestamp(0) NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for categories
+-- ----------------------------
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '栏目名称',
+  `sort` int(11) NULL DEFAULT 1 COMMENT '栏目排序',
+  `parent_id` int(11) NULL DEFAULT NULL COMMENT '父级栏目ID',
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '栏目状态:0-正常/1-隐藏',
+  `created_at` timestamp(0) NOT NULL COMMENT '创建时间',
+  `updated_at` timestamp(0) NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for link
+-- ----------------------------
+DROP TABLE IF EXISTS `link`;
+CREATE TABLE `link`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `link_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '链接名称',
+  `link_url` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '链接url',
+  `icon_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '链接图标地址',
+  `sort` int(11) NULL DEFAULT 1 COMMENT '链接排序',
+  `created_at` timestamp(0) NOT NULL COMMENT '创建时间',
+  `updated_at` timestamp(0) NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `passport` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户账号',
+  `password` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户密码',
+  `nickname` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户昵称',
+  `email` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '邮箱',
+  `summary` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '自我简介',
+  `avatar` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '头像地址',
+  `we_chat` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '微信号',
+  `last_login_time` timestamp(0) NOT NULL COMMENT '最后登录时间',
+  `last_login_ip` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '最后登录IP',
+  `created_at` timestamp(0) NOT NULL COMMENT '创建时间',
+  `updated_at` timestamp(0) NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
